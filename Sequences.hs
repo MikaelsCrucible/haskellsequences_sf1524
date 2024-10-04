e98c04f4a914291b7dcfd5293d8333520a81d25a
Sihan Fu <sf1524@ic.ac.uk>
2024-10-04 01:07:30 +0100

    Implementation of the utility functions

--- Sequences.hs
+++ Sequences.hs
@@ -1,38 +1,43 @@
 module Sequences where
-
 import Data.Char (ord, chr)
-
 -- | Returns the first argument if it is larger than the second,
 -- the second argument otherwise
 maxOf2 :: Int -> Int -> Int
-maxOf2 = undefined
-
+maxOf2 x y
+  | x > y = x
+  | otherwise = y
+  
 -- | Returns the largest of three Ints
 maxOf3 :: Int -> Int -> Int -> Int
-maxOf3 = undefined
-
+maxOf3 x y z = if x >= maxOf2 y z then
+  x
+  else
+  maxOf2 y z
 -- | Returns True if the character represents a digit '0'..'9';
 -- False otherwise
 isADigit :: Char -> Bool
-isADigit = undefined
-
+isADigit x
+  | x >= '0' && x <= '9' = True
+  | otherwise = False
 -- | Returns True if the character represents an alphabetic
 -- character either in the range 'a'..'z' or in the range 'A'..'Z';
 -- False otherwise
 isAlpha :: Char -> Bool
-isAlpha = undefined
-
+isAlpha x
+  | x >= 'a' && x <= 'z' = True
+  | x >= 'A' && x <= 'Z' = True
+  | otherwise = False
 -- | Returns the integer [0..9] corresponding to the given character.
 -- Note: this is a simpler version of digitToInt in module Data.Char,
 -- which does not assume the precondition.
 digitToInt :: Char -> Int
+digitToInt x = ord x - ord '0'
 -- Pre: the character is one of '0'..'9'
-digitToInt = undefined
 
 -- | Returns the upper case character corresponding to the input.
 -- Uses guards by way of variety.
 toUpper :: Char -> Char
-toUpper = undefined
+toUpper x = chr( ord x - ( ord 'a' - ord 'A' ) )
 
 --
 -- Sequences and series
